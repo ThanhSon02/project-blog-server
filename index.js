48c547ab47da02391c785aac31e909e7f4d0c1c1
ThanhSon02 <thanhson.hw@gmail.com>
2023-03-15 16:22:39 +0700

    create login and logout feature

--- index.js
+++ index.js
@@ -4,17 +4,19 @@ const mongoose = require("mongoose");
 const User = require("./models/User");
 const brypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 
 const salt = brypt.genSaltSync(10);
 const secret = "nckasnfor32esfse35w";
 
-app.use(cors());
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 app.use(express.json());
+app.use(cookieParser());
 
 mongoose.connect(
-    "mongodb+srv://blog-admin:dlNAf8b6TfZ8DjB6@cluster0.q9fs7ze.mongodb.net/?retryWrites=true&w=majority",
+    "mongodb+srv://project-blog:GDxAcHUds0a7hrBk@project-blog.lgc68qk.mongodb.net/?retryWrites=true&w=majority",
 );
 
 app.post("/register", async (req, res) => {
@@ -38,13 +40,26 @@ app.post("/login", async (req, res) => {
         // logged in
         jwt.sign({ username, id: userDoc._id }, secret, {}, (err, token) => {
             if (err) throw err;
-            res.cookie("token", token).json("ok");
+            res.cookie("token", token).json({
+                id: userDoc._id,
+                username,
+            });
         });
     } else {
         res.status(400).json("wrong credentials");
     }
 });
 
+app.get("/profile", (req, res) => {
+    const { token } = req.cookies;
+    jwt.verify(token, secret, {}, (err, info) => {
+        if (err) throw err;
+        res.json(info);
+    });
+});
+
+app.post("/logout", (req, res) => {
+    res.cookie("token", "").json("ok");
+});
+
 app.listen(4000);
-// dlNAf8b6TfZ8DjB6
-// mongodb+srv://blog-admin:dlNAf8b6TfZ8DjB6@cluster0.q9fs7ze.mongodb.net/?retryWrites=true&w=majority
