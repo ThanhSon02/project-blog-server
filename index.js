fc31a2faafd7b9e23bbba6ea35a2aeb0a2eb9342
ThanhSon02 <thanhson.hw@gmail.com>
2023-03-16 15:25:46 +0700

    create and display posts

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ const User = require("./models/User");
 const brypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const cookieParser = require("cookie-parser");
-
+const multer = require("multer");
+const uploadMiddleware = multer({ dest: "uploads/" });
+const fs = require("fs");
+const Post = require("./models/Post");
 const app = express();
 
 const salt = brypt.genSaltSync(10);
@@ -14,6 +17,7 @@ const secret = "nckasnfor32esfse35w";
 app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 app.use(express.json());
 app.use(cookieParser());
+app.use("/uploads", express.static(__dirname + "/uploads"));
 
 mongoose.connect(
     "mongodb+srv://project-blog:GDxAcHUds0a7hrBk@project-blog.lgc68qk.mongodb.net/?retryWrites=true&w=majority",
@@ -60,6 +64,39 @@ app.get("/profile", (req, res) => {
 
 app.post("/logout", (req, res) => {
     res.cookie("token", "").json("ok");
+});
+
+app.post("/post", uploadMiddleware.single("file"), async (req, res) => {
+    const { originalname, path } = req.file;
+    const parts = originalname.split(".");
+    const ext = parts[parts.length - 1];
+    const newPath = path + "." + ext;
+
+    fs.renameSync(path, newPath);
+
+    const { token } = req.cookies;
+    jwt.verify(token, secret, {}, async (err, info) => {
+        if (err) throw err;
+        const { title, summary, content } = req.body;
+        const postDoc = await Post.create({
+            title,
+            summary,
+            content,
+            cover: newPath,
+            author: info.id,
+        });
+
+        res.json(postDoc);
+    });
+});
+
+app.get("/post", async (req, res) => {
+    res.json(
+        await Post.find()
+            .populate("author", ["username"])
+            .sort({ createdAt: -1 })
+            .limit(20),
+    );
 });
 
 app.listen(4000);
